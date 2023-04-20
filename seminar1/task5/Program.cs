0b65b05279c4c24c960da45c0d02e2f8e6db8bbc
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-20 22:58:21 +0400

    task 5 fix

--- seminar1/task5/Program.cs
+++ seminar1/task5/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the number: ");
+string a = Console.ReadLine();
+int xa = Convert.ToInt32(a);
+if(xa > 99 && xa < 999){
+int b = xa%10;
+Console.WriteLine(b);
+} else {
+    Console.WriteLine("You entered wrong number");
+}
