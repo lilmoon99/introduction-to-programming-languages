9b2ea86eda5bb508b163887e7dc51f81a6253bc3
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-22 10:08:58 +0400

    task 2 solved

--- seminar1_homeWork/task2/Program.cs
+++ seminar1_homeWork/task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+Console.WriteLine("We will compare three numbers!");
+Console.Write("Enter first number: ");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+int third_number = Convert.ToInt32(Console.ReadLine());
+int max = first_number;
+    if(second_number > max){
+        max = second_number;
+    }
+    if(third_number > max){
+        max = third_number;
+    }
+Console.WriteLine("Max is: " + max);
