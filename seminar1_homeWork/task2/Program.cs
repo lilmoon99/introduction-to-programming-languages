87fe2c95412825d3da6129ef4cb969ec717d1abc
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-22 11:12:40 +0400

    task 2 try/catch added

--- seminar1_homeWork/task2/Program.cs
+++ seminar1_homeWork/task2/Program.cs
@@ -5,17 +5,32 @@
 22 3 9 -> 22
 */
 Console.WriteLine("We will compare three numbers!");
-Console.Write("Enter first number: ");
-int first_number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter second number: ");
-int second_number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter third number: ");
-int third_number = Convert.ToInt32(Console.ReadLine());
-int max = first_number;
-    if(second_number > max){
-        max = second_number;
+bool is_need_launch_more = true;
+while (is_need_launch_more)
+{
+    try
+    {
+        Console.Write("Enter first number: ");
+        int first_number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int second_number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter third number: ");
+        int third_number = Convert.ToInt32(Console.ReadLine());
+        int max = first_number;
+        if (second_number > max)
+        {
+            max = second_number;
+        }
+        if (third_number > max)
+        {
+            max = third_number;
+        }
+        Console.WriteLine("Max is: " + max);
+        is_need_launch_more = false;
     }
-    if(third_number > max){
-        max = third_number;
+    catch
+    {
+        FormatException exception;
+        Console.WriteLine("You entered not a number. Please try again ...");
     }
-Console.WriteLine("Max is: " + max);
+}
