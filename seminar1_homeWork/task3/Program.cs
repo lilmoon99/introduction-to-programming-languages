0b04f7f5a7676da22b9b81257b7a91f004ed0a94
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-22 10:38:16 +0400

    task 3 solved

--- seminar1_homeWork/task3/Program.cs
+++ seminar1_homeWork/task3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.WriteLine("Even or odd? Let's check!");
+int numberToCheck = 0;
+bool isNotDone = true;
+while (isNotDone)
+{
+    try
+    {
+        Console.Write("Enter a number: ");
+        string readLine = Console.ReadLine();
+        numberToCheck = Convert.ToInt32(readLine);
+        if (numberToCheck % 2 == 0)
+        {
+            Console.WriteLine("Your number is even.");
+        }
+        else
+        {
+            Console.WriteLine("Your number is odd.");
+        }
+        isNotDone = false;
+    }
+    catch
+    {
+        FormatException exception;
+        Console.WriteLine("You entered wrong number. Please try again...");
+    }
+}
