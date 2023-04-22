8307311049d595ac38af1cb64105821c42725b74
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-22 11:21:19 +0400

    task 4 try/catch added

--- seminar1_homeWork/task4/Program.cs
+++ seminar1_homeWork/task4/Program.cs
@@ -3,10 +3,26 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
-Console.Write("Enter a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-for(int i = 1;i <= number;i++){
-    if(i % 2 == 0){
+bool is_need_launch_more = true;
+int number = 0;
+while (is_need_launch_more)
+{
+    try
+    {
+        Console.Write("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        is_need_launch_more = false;
+    }
+    catch
+    {
+        FormatException exception;
+        Console.WriteLine("You entered not a number. Please try again ...");
+    }
+}
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0)
+    {
         Console.Write(i + " ");
     }
 }
