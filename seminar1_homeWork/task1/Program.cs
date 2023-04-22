710dfc83786eab7aece23410f22fa47ec0df2ab0
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-22 11:07:45 +0400

    task 1 try/catch added

--- seminar1_homeWork/task1/Program.cs
+++ seminar1_homeWork/task1/Program.cs
@@ -5,14 +5,28 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 Console.WriteLine("We will compare two numbers!");
-Console.Write("Enter first number: ");
-int first_number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter second number: ");
-int second_number = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-if(first_number > second_number){
-    max = first_number;
-}else{
-    max = second_number;
+bool is_need_launch_more = true;
+while (is_need_launch_more)
+{
+    try
+    {
+        Console.Write("Enter first number: ");
+        int first_number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int second_number = Convert.ToInt32(Console.ReadLine());
+        int max = 0;
+        if (first_number > second_number)
+        {
+            max = first_number;
+        }
+        else
+        {
+            max = second_number;
+        }
+        Console.WriteLine("max is: " + max);
+        is_need_launch_more = false;
+    } catch {
+        FormatException exception;
+        Console.WriteLine("You entered not a number. Please try again ...");
+    }
 }
-    Console.WriteLine("max is: " + max);
