ded8137404da70936c7334134d1f838c8a33539a
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-24 22:03:57 +0400

    add seminar 2 task 3 and task 4

---
+++ seminar2/task4/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно
+
+Задача №14.
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+7 и 23.
+
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+System.Console.WriteLine("Enter a first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter a second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber % secondNumber == 0 || secondNumber % firstNumber == 0){
+    System.Console.WriteLine("True");
+} else {
+        System.Console.Write($"False. ");
+    if (firstNumber > secondNumber)
+    {
+        System.Console.WriteLine(firstNumber % secondNumber);
+    } else
+    {
+        System.Console.WriteLine(secondNumber % firstNumber);
+    }
+
+}
---
+++ seminar2/task5/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача №14.
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+7 и 23.
+
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+
+System.Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDiv = 7;
+int secondDiv = 23;
+if(number % firstDiv == 0 && number% secondDiv == 0){
+    System.Console.WriteLine("True");
+} else
+{
+    System.Console.WriteLine("False");
+}
