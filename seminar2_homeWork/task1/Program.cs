cdf810ebadb05acc26f56af01bb86dd98371ac74
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-24 22:26:01 +0400

    task 1 solved

--- seminar2_homeWork/task1/Program.cs
+++ seminar2_homeWork/task1/Program.cs
@@ -4,3 +4,11 @@
 782 -> 8
 918 -> 1
 */
+
+System.Console.WriteLine("Enter a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    System.Console.WriteLine((number % 100)/10);
+}
