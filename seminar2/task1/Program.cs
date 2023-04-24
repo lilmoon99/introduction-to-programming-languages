1056cd0678aad4abf7b3640e5acd8be701154f75
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-24 21:23:42 +0400

    added seminar 2 task 1

---
+++ seminar2/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Random rand = new Random();
+int randomNumber = rand.Next(10, 100);
+System.Console.WriteLine(randomNumber);
+int a = randomNumber / 10;
+int b = randomNumber % 10;
+if (a > b)
+{
+    System.Console.WriteLine($"Our number is {randomNumber}. {a} > {b}. Result {a}");
+}
+else if (a < b)
+{
+    System.Console.WriteLine($"Our number is {randomNumber}. {b} > {a}. Result {b}");
+}
+else
+{
+    System.Console.WriteLine($"Our number is {randomNumber}. {a} = {b}");
+}
