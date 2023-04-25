d1a5d252c71b093462cee006c37cb78570b332b9
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-25 20:38:32 +0400

    task 3 solved

--- seminar2_homeWork/task3/Program.cs
+++ seminar2_homeWork/task3/Program.cs
@@ -4,3 +4,18 @@
 7 -> да
 1 -> нет
 */
+
+Console.WriteLine("Enter a day number: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber > 7 || dayNumber < 1)
+{
+    System.Console.WriteLine("You entered wrong day number. Please enter a number from 1 to 7.");
+}
+else if (dayNumber > 5 && dayNumber <= 7)
+{
+    System.Console.WriteLine("It's time to chill, bro!");
+} else {
+    System.Console.WriteLine("You have to wake up and go to work,looser!");
+}
+
