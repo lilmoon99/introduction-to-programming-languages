d8e0091672f87fe25577863a1c1953176e7513fd
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-25 20:29:03 +0400

    task 2 solved

--- seminar2_homeWork/task2/Program.cs
+++ seminar2_homeWork/task2/Program.cs
@@ -4,3 +4,22 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
+
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int divider = 1000;
+int counter = 0;
+if (number < 99)
+{
+    System.Console.WriteLine("You dont have third digit on your number.");
+} else{
+while (number > divider) // определяем к какому промежутку чисел принадлежит число 
+{
+    divider = divider * 10;
+    counter++;
+}
+for (int i = 0; i < counter;i++){ // делаем number трехзначным
+    number = number / 10;
+}
+System.Console.WriteLine(number % 10); // находим последнее число
+}
