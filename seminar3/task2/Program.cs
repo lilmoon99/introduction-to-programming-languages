ec6e6c2241a01e2b9e26f22366e5f76ce13f031f
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-27 21:41:41 +0400

    add task 2 and task 3

--- seminar3/task2/Program.cs
+++ seminar3/task2/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿/*
+Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+Console.WriteLine("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+if (quarter > 4)
+{
+    System.Console.WriteLine("You entered wrong quarter!");
+}
+switch (quarter)
+{
+    case 1:
+        System.Console.WriteLine("X > 0, Y > 0");
+        break;
+    case 2:
+        System.Console.WriteLine("X > 0, Y < 0");
+        break;
+    case 3:
+        System.Console.WriteLine("X < 0, Y < 0");
+        break;
+    case 4:
+        System.Console.WriteLine("X < 0, Y > 0");
+        break;
+}
---
+++ seminar3/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача №22. Работа в группах
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number;i++){
+    System.Console.Write(i*i + " ");
+}
