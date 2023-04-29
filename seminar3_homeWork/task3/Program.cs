df90c83a0e319c7681263eeb4fa4b077005917f9
Aynur Shamsullin <lilmoon99@ya.ru>
2023-04-29 12:42:48 +0400

    task 3 solved

--- seminar3_homeWork/task3/Program.cs
+++ seminar3_homeWork/task3/Program.cs
@@ -3,3 +3,33 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+int number = 0;
+try
+{
+    Console.WriteLine("Enter a number: ");
+number = Convert.ToInt32(Console.ReadLine());
+}
+catch (System.FormatException)
+{
+    System.Console.WriteLine("You entered not a number!");
+}
+coubFinder(number);
+
+
+
+
+void coubFinder(int number)
+{
+    if (number > 0)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            System.Console.Write(Math.Pow(i + 1, 3) + " ");
+        }
+    } else {
+        for (int i = 0; i < number*(-1); i++)
+        {
+            System.Console.Write((Math.Pow(i + 1, 3))*(-1) + " ");
+        }
+    }
+}
