e41fdd94af8cba2e66b6f1674c9cd5f3cf1a28a8
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-17 13:27:13 +0400

    example 012

--- lecture3/Example012_Methods/Program.cs
+++ lecture3/Example012_Methods/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+
+// Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+
+// Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        System.Console.WriteLine(msg);
+        i++;
+    }
+}
+// Method21(msg: "Текст", count: 4);
+// Method21(count: 4,msg: "Новый текст");
+
+int Method3(){
+    return DateTime.Now.Year;
+}
+// int year = Method3();
+// System.Console.WriteLine(year);
+
+string Method4(int count,string text){
+    int i = 0;
+    string result = String.Empty;
+    while(i < count){
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10,"z");
+// System.Console.WriteLine(res);
