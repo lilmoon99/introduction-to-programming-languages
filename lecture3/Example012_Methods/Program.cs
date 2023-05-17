1784326b9580a4df38a0339a9f86f7d21f03f28b
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-17 14:03:16 +0400

    example 012 with hometask

--- lecture3/Example012_Methods/Program.cs
+++ lecture3/Example012_Methods/Program.cs
@@ -26,21 +26,111 @@ void Method21(string msg, int count)
 // Method21(msg: "Текст", count: 4);
 // Method21(count: 4,msg: "Новый текст");
 
-int Method3(){
+int Method3()
+{
     return DateTime.Now.Year;
 }
 // int year = Method3();
 // System.Console.WriteLine(year);
 
-string Method4(int count,string text){
-    int i = 0;
+string Method4(int count, string text)
+{
+    // int i = 0;
     string result = String.Empty;
-    while(i < count){
+    // while(i < count){
+    //     result = result + text;
+    //     i++;
+    // }
+    for (int i = 0; i < count; i++)
+    {
         result = result + text;
-        i++;
     }
     return result;
 }
 
-string res = Method4(10,"z");
+string res = Method4(10, "z");
 // System.Console.WriteLine(res);
+
+// for(int i = 2;i < 10;i++){
+//     for(int j = 2;j < 10;j++){
+//         System.Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+
+//     System.Console.WriteLine();
+// }
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что"
+        + "ежели бы вас послали вместо нашего милого Винценгероде,"
+        + "вы бы взяли приступом согласие прусского короля. "
+        + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = "qwerty";
+//             012345
+// s[3] = r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+// string newText = Replace(text, ' ','|');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+// newText = Replace(newText,'к','К');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+// newText = Replace(newText,'с','С');
+// System.Console.WriteLine(newText);
+
+int [] arr = {1,5,4,3,2,6,7,1,1,};
+
+void PrintArray(int []array){
+    int count = array.Length;
+    for(int i = 0;i< count;i++){
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSortMinToMax(int [] array){
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+void SelectionSortMaxToMin(int [] array){
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSortMinToMax(arr);
+PrintArray(arr);
+SelectionSortMaxToMin(arr);
+PrintArray(arr);
