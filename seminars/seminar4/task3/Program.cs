bf58112b202c636d0a2f111f55752b391b65141c
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-18 21:45:21 +0400

    task 2 and task 3 solved

--- seminars/seminar4/task3/Program.cs
+++ seminars/seminar4/task3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+*/
+System.Console.WriteLine("Enter a number: ");
+int numberEntered = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Multiply(numberEntered));
+int Multiply(int number){
+    int result = 1;
+    for(int i = 1; i <= number;i++){
+        result *= i;
+    }
+    return result;
+}
