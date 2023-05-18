e4a4da7250c5eee0213a09063fb33ba0deea8852
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-18 21:12:43 +0400

    task 1 solved

---
+++ seminars/seminar4/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+System.Console.WriteLine("Enter a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{a} -> {Sum(a)}");
+
+int Sum(int a){
+    int result = 0;
+    for(int i = 0;i <= a;i++){
+        result += i;
+    }
+    return result;
+}
