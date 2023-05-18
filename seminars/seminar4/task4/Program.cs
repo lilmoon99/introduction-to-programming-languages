77297ae6d6f3fd0d6cd5338e0c55b887ffb1e0d3
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-18 22:31:11 +0400

    task 4 solved

--- seminars/seminar4/task4/Program.cs
+++ seminars/seminar4/task4/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+int [] array = randomArrayGenerator();
+ArrayPrint1(array);
+System.Console.WriteLine();
+ArrayPrint2(array);
+
+int [] randomArrayGenerator(){
+    int [] tempArray = new int [8];
+    Random random = new Random();
+    for(int i = 0; i < tempArray.Length;i++){
+        tempArray[i] = random.Next(0,2);
+    }
+    return tempArray;
+}
+
+void ArrayPrint1(int [] array){
+    for(int i = 0;i < array.Length;i++){
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+void ArrayPrint2(int [] array){
+    System.Console.WriteLine(String.Join(" ",array));
+    
+}
