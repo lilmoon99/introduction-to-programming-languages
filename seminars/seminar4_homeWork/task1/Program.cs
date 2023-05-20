2348506577316e93aa367b1a484139f559ecd600
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-20 09:59:54 +0400

    task 1 solved

--- seminars/seminar4_homeWork/task1/Program.cs
+++ seminars/seminar4_homeWork/task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+System.Console.WriteLine("Enter a first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter a second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"{firstNumber}, {secondNumber} -> {Pow(firstNumber,secondNumber)}");
+
+double Pow(int a,int b){
+    return Math.Pow(a,b);
+}
