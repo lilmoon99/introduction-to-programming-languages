e8c4574fa428b552b0486b1f0e54c0b0593e6e93
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-20 10:23:17 +0400

    task 2 solved

--- seminars/seminar4_homeWork/task2/Program.cs
+++ seminars/seminar4_homeWork/task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+System.Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{number} -> {DigitSum(number)}");
+ int DigitSum(int arg){
+    int result = 0;
+    while(arg > 0){
+        result = result + arg % 10;
+        arg /= 10;
+    }
+    return result;
+ }
