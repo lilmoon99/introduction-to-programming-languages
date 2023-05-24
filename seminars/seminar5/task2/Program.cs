9d6531bc46eef220f57c3063027225324a2d703e
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-24 11:15:49 +0400

    task 2 solved

--- seminars/seminar5/task2/Program.cs
+++ seminars/seminar5/task2/Program.cs
@@ -1,7 +1,22 @@
 ﻿/*
 Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 [-4, -8, 8, 2] -> [4, 8, -8, -2]
-Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
-4; массив [6, 7, 19, 345, 3] -> нет
--3; массив [6, 7, 19, 345, -3] -> да
 */
+
+int[] array = GenerateArray(10);
+System.Console.WriteLine(String.Join(',',array));
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= (-1);
+}
+System.Console.WriteLine(String.Join(',',array));
+
+int[] GenerateArray(int length)
+{
+    int[] tempArray = new int[length];
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        tempArray[i] = new Random().Next(-10, 11);
+    }
+    return tempArray;
+}
