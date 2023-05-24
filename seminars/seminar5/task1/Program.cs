15731952e0d09599cea78eacd2827a249a355aa9
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-24 11:07:39 +0400

    task 1 solved

--- seminars/seminar5/task1/Program.cs
+++ seminars/seminar5/task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+*/
+
+int[] array = GenerateArray();
+int sumPositive = 0;
+int sumNegative = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) sumPositive += array[i];
+    else sumNegative += array[i];
+}
+System.Console.WriteLine(String.Join(',',array));
+System.Console.WriteLine($"Сумма положительных чисел: {sumPositive}");
+System.Console.WriteLine($"Сумма отрицательных чисел: {sumNegative}");
+
+
+int[] GenerateArray()
+{
+    int[] array = new int[12];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    return array;
+}
