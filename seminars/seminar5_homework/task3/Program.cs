bbbf0f83b4c8739ca5c6e6a23df26c44d125f3e9
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-24 15:18:01 +0400

    task 2 solved

--- seminars/seminar5_homework/task3/Program.cs
+++ seminars/seminar5_homework/task3/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Диапазон [-10, 10]. Обратите внимание на вещественных чисел
+Для примера возьмём диапазон побольше. [3 7 22 2 78] -> 76
+*/
+int length = 10;
+double[] array = GenerateArray(length);
+System.Console.WriteLine($"[{String.Join('|', array)}] -> {MaxMinSub(array)}");
+
+double[] GenerateArray(int length)
+{
+    double[] tempArray = new double[length];
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        tempArray[i] = RandomDoubleInRange(-10, 10);
+        tempArray[i] = Math.Round(tempArray[i], 2);
+    }
+    return tempArray;
+}
+double RandomDoubleInRange(double min, double max)
+{
+    Random rand = new Random();
+    double range = max - min;
+    double sample = rand.NextDouble();
+    double scaled = (sample * range) + min;
+    return scaled;
+}
+
+double MaxMinSub(double[] array)
+{
+    double minValue = array[0];
+    double maxValue = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length; j++)
+        {
+            if(minValue > array[j]) minValue = array[j];
+        }
+        if(maxValue < array[i]) maxValue = array[i];
+    }
+    System.Console.WriteLine($"max = {maxValue},min = {minValue}");
+    return maxValue - minValue;
+}
+
