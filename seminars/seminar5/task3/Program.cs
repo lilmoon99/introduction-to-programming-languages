aefb16ff684a831f93aa821abe971b8a40e148fe
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-24 11:25:41 +0400

    task 3 solved

--- seminars/seminar5/task3/Program.cs
+++ seminars/seminar5/task3/Program.cs
@@ -3,3 +3,19 @@
 4; массив [6, 7, 19, 345, 3] -> нет
 -3; массив [6, 7, 19, 345, -3] -> да
 */
+
+int[] array = { 6, 7, 19, 345, -3 };
+System.Console.WriteLine(String.Join(' ',array));
+System.Console.WriteLine(SearchNumber(array,-3));
+bool SearchNumber(int[] array, int numberToSearch)
+{
+    bool isContain = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == numberToSearch){
+            isContain = true;
+            break;
+        }
+    }
+    return isContain;
+}
