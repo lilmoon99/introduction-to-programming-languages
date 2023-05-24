8cb834aae415795ca245fd4bc3717871bce853c2
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-24 13:10:24 +0400

    task 2 solved

--- seminars/seminar5_homework/task2/Program.cs
+++ seminars/seminar5_homework/task2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int length = 8;
+int[] array = GenerateArray(length);
+System.Console.WriteLine($"[{String.Join(',', array)}] -> {SumOfOddElements(array)}");
+
+
+int[] GenerateArray(int length)
+{
+    int[] tempArray = new int[length];
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        tempArray[i] = new Random().Next(0, 20);
+    }
+    return tempArray;
+}
+
+int SumOfOddElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
