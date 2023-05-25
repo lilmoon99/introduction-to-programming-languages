7b5484f978fb47e643fbc6a49c27a851e4a15c65
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-25 22:20:15 +0400

    task 4 and task 5 solved

--- seminars/seminar6/task4/Program.cs
+++ seminars/seminar6/task4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+int number = 10;
+System.Console.WriteLine(string.Join(',',Febonachi(number)));
+
+int[] Febonachi(int N){
+    int[] febonachiArray = new int[N];
+    febonachiArray[0] = 0;
+    febonachiArray[1] = 1;
+    for (int i = 2; i < febonachiArray.Length; i++)
+    {
+        febonachiArray[i] = febonachiArray[i-1] + febonachiArray[i-2];
+    }
+    return febonachiArray;
+}
--- seminars/seminar6/task5/Program.cs
+++ seminars/seminar6/task5/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая будет создавать копию 
+заданного массива с помощью поэлементного копирования.
+*/
+System.Console.WriteLine("Enter a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] generatedArray = GenerateArray(length);
+System.Console.WriteLine($"{String.Join(',',generatedArray)} -> generated array");
+int[] copiedArray = CopyArray(generatedArray);
+System.Console.WriteLine($"{String.Join(',',copiedArray)} -> copied array");
+
+
+int[] GenerateArray(int length){
+    int[] tempArray = new int[length];
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        tempArray[i] = new Random().Next(0,100);
+    }
+    return tempArray;
+}
+
+int[] CopyArray(int[] array){
+    int[] tempArray = new int[array.Length];
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        tempArray[i] = array[i];
+    }
+    return tempArray;
+}
