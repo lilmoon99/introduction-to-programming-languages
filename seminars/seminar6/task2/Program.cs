48bd236944ca4ae3141d85717bbb6db82da14bba
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-25 22:04:36 +0400

    task 4 init commit

--- seminars/seminar6/task2/Program.cs
+++ seminars/seminar6/task2/Program.cs
@@ -2,3 +2,28 @@
 Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
 Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
 */
+
+
+
+
+int[] CreateSides(string message)
+{
+    System.Console.WriteLine(message);
+        int[] tempArray = new int [3];
+
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        System.Console.WriteLine($"Сторона {i+1}: ");
+        tempArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return tempArray;
+}
+
+bool IsTriangle(int[] array){
+    if(array[0] < array[1]+ array[2] && array[1]<array[0] + array[2] && array[2] < array[0]+ array[1]){
+        return true;
+    } else return false;
+}
+
+int[] array = CreateSides("Введите стороны треугольника...");
+System.Console.WriteLine(IsTriangle(array));
--- seminars/seminar6/task3/Program.cs
+++ seminars/seminar6/task3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+
+string ConvertToBinary(int number)
+{
+    string result = "";
+    while (number > 0)
+    {
+        result = number % 2 + result;
+        number /= 2;
+    }
+    return result;
+}
+System.Console.WriteLine("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(ConvertToBinary(num));
