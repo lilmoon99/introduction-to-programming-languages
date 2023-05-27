61ebf0b5e32841bbd255ca3936f3c5078861f158
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-27 16:59:03 +0400

    Example 013

--- lectures/lecture4/Example013_RecursionAlgorithm/Program.cs
+++ lectures/lecture4/Example013_RecursionAlgorithm/Program.cs
@@ -1,2 +1,130 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[,] table = new string[2, 5];
+// table[0,0]   table[0,1]  table[0,2]  table[0,3]  table[0,4]
+// table[1,0]   table[1,1]  table[1,2]  table[1,3]  table[1,4]
+// table[1,2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         System.Console.WriteLine($"-{table[rows,columns]}-");
+//     }
+// }
+
+
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             System.Console.Write($"{matr[i, j]} ");
+//         }
+//         System.Console.WriteLine();
+//     }
+
+// }
+
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+// // int[,] matrix = new int[3, 4];
+// // PrintArray(matrix);
+// // FillArray(matrix);
+// // System.Console.WriteLine();
+// // PrintArray(matrix);
+
+// int[,] pic = new int[,]
+// {
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+//  {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+// void PrintImage(int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             if (image[i, j] == 0) System.Console.WriteLine(" ");
+//             else System.Console.WriteLine("+");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void FillImage(int row, int col)
+// {
+//     if (pic[row, col] == 0)
+//     {
+//         pic[row, col] = 1;
+//         FillImage(row - 1, col);
+//         FillImage(row, col - 1);
+//         FillImage(row + 1, col);
+//         FillImage(row, col + 1);
+
+//     }
+// }
+
+// PrintImage(pic);
+// FillImage(13,13);
+// PrintArray(pic);
+
+
+
+
+
+
+
+// int Factorial(int n){
+//     if(n ==1) return 1;
+//     else return n*Factorial(n-1);
+// }
+// for (int i = 0; i < 10; i++)
+// {
+//     System.Console.WriteLine(Factorial(i));
+// }
+
+
+//f(1) = 1;
+//f(2) = 1;
+//f(n) = f(n-1) + f(n-2)
+
+int Febonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Febonacci(n-1) + Febonacci(n-2);
+}
+for (int i = 0; i < 5; i++)
+{
+    System.Console.WriteLine(Febonacci(i));
+}
