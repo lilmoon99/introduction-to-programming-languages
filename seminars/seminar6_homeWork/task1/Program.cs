c7d5762d169c1e2c22e1b4de0a31ad5c3ef81c69
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-27 11:53:43 +0400

    task 1 solved

--- seminars/seminar6_homeWork/task1/Program.cs
+++ seminars/seminar6_homeWork/task1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Enter a numbers.For example: 1,2,3,4,5,6");
+string numbers = Console.ReadLine();
+bool IsNotANumber = false;
+string[] arrayOfStr = numbers.Split(',',' ','.','/','|');
+int[] numbersArray = CopyToIntArray(arrayOfStr);
+
+int[] CopyToIntArray(string[] array)
+{
+    int[] tempArray = new int[array.Length];
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        try
+        {
+            tempArray[i] = Convert.ToInt32(array[i]);
+        }
+        catch (FormatException)
+        {
+            System.Console.WriteLine($"{array[i]} is not a number.Please try again.");
+            IsNotANumber = true;
+        }
+        if (IsNotANumber)
+        {
+            break;
+        }
+    }
+    return tempArray;
+}
+
+if (IsNotANumber == false)
+{
+    System.Console.WriteLine($"{String.Join('|', CopyToIntArray(arrayOfStr))}");
+    NumbersGreaterThanZero(numbersArray);
+}
+
+
+void NumbersGreaterThanZero(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            counter++;
+        }
+    }
+    System.Console.WriteLine($"There is {counter} numbers greater than zero(0).");
+}
+
+
+
