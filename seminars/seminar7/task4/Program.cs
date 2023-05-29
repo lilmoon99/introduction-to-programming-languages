b69f0aa7cf361b04f45102e5f11f6059af7ddad7
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-29 22:03:57 +0400

    task4

--- seminars/seminar7/task4/Program.cs
+++ seminars/seminar7/task4/Program.cs
@@ -6,3 +6,57 @@
 8 4 2 4
 Сумма элементов главной диагонали: 1+9+2 = 12
 */
+
+int[,] FillArray(int row, int column)
+{
+    int[,] tempArray = new int[row, column];
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArray.GetLength(1); j++)
+        {
+            tempArray[i, j] = GenerateNumber();
+        }
+    }
+    return tempArray;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int GenerateNumber()
+{
+
+    int temp = new Random().Next(0, 100);
+    return temp;
+}
+
+int DiagonalSum(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); i++)
+        {
+            if (i == j)
+            {
+                result = result + array[i,j];
+            }
+        }
+    }
+    return result;
+}
+
+int row = 4;
+int column = 4;
+int[,] ourArray = FillArray(row, column);
+PrintArray(ourArray);
+System.Console.WriteLine(DiagonalSum(ourArray));
