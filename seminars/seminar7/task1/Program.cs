c13f49e9132552c23fac833d984bb868b030b524
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-29 21:16:31 +0400

    task 1 solved

--- seminars/seminar7/task1/Program.cs
+++ seminars/seminar7/task1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+int GenerateNumber()
+{
+
+    int temp = new Random().Next(0, 100);
+    return temp;
+}
+
+int[,] FillArray(int row, int column)
+{
+    int[,] tempArray = new int[row, column];
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArray.GetLength(1); j++)
+        {
+            tempArray[i, j] = GenerateNumber();
+        }
+    }
+    return tempArray;
+}
+
+void PrintArray(int[,] array){
+        for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int row = 3;
+int column = 4;
+int[,] ourArray = FillArray(row,column);
+PrintArray(ourArray);
