a68e852010565354f36b1cb411afea05c4f3f78e
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-29 21:00:41 +0400

    lecture 7 Recursion

--- lectures/lecture7/RecTreeExample/Program.cs
+++ lectures/lecture7/RecTreeExample/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2,", "1", "3" };
+
+void InOrderTravelsal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTravelsal(left);
+        System.Console.WriteLine(tree[pos]);
+        if(right<tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTravelsal(right);
+    }
+}
