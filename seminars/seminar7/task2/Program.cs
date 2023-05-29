3e91f997869256d80a106ae7618e4647d7ea0862
Aynur Shamsullin <lilmoon99@ya.ru>
2023-05-29 21:24:49 +0400

    task 2 solved

--- seminars/seminar7/task2/Program.cs
+++ seminars/seminar7/task2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+int[,] FillArray(int row, int column)
+{
+    int[,] tempArray = new int[row, column];
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArray.GetLength(1); j++)
+        {
+            tempArray[i, j] = i+j;
+        }
+    }
+    return tempArray;
+}
+
+void PrintArray(int[,] array){
+        for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = 3;
+int column = 4;
+int[,] ourArray = FillArray(row,column);
+PrintArray(ourArray);
