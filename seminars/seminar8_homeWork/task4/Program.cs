7bf99063377cd62fc6bc579845f03ce9ab083399
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-05 15:53:42 +0400

    task 4 solved

--- seminars/seminar8_homeWork/task4/Program.cs
+++ seminars/seminar8_homeWork/task4/Program.cs
@@ -7,84 +7,141 @@
 10 09 08 07
 */
 
-int[,] matrix;
-int N;
-string answer;
+// int[,] matrix;
+// int N;
+// string answer;
 
-void InputMatrix()
+// void InputMatrix()
+// {
+//     N = int.Parse(Console.ReadLine());
+//     matrix = new int[N, N];
+//     for (int y = 0; y < N; y++)
+//     {
+//         string[] line = Console.ReadLine().Split();
+//         for (int x = 0; x < N; x++)
+//         {
+//             matrix[y, x] = int.Parse(line[x]);
+//         }
+//     }
+// }
+
+// void InputTestMatrix()
+// {
+//     N = 5;
+//     // matrix = new int[5, 5] {
+//     //     {1,2,3,4,5},
+//     //     {4,5,6,7,8},
+//     //     {7,8,9,1,2},
+//     //     {8,7,6,5,3},
+//     //     {4,6,7,9,8}};
+//             matrix = new int[5, 5] {
+//         {0,0,0,0,0},
+//         {0,0,0,0,0},
+//         {0,0,0,0,0},
+//         {0,0,0,0,0},
+//         {0,0,0,0,0}};
+// }
+
+// void SpiralePath()
+// {
+//     int x = 0;
+//     int y = 0;
+//     int sx = 1;
+//     int sy = 0;
+//     answer = matrix[y, x].ToString();
+
+//     matrix[y, x] = -1;
+//     bool found;
+//     do
+//     {
+//         found = false;
+//         while (InRange(x + sx, y + sy))
+//         {
+//             x += sx;
+//             y += sy;
+//             answer += " " + matrix[y, x];
+//             matrix[y, x] = -1;
+//             found = true;
+//         }
+//         int sxTemp = sx;
+//         sx = -sy;
+//         sy = sxTemp;
+//         System.Console.WriteLine(answer);
+//         Console.ReadKey();
+//     } while (found);
+// }
+
+// bool InRange(int x, int y)
+// {
+//     if (x < 0 || x >= N) return false;
+//     if (y < 0 || y >= N) return false;
+//     if (matrix[y, x] == -1) return false;
+//     return true;
+// }
+
+// void WriteAnswer()
+// {
+//     System.Console.WriteLine(answer);
+//     System.Console.ReadKey();
+// }
+
+// InputTestMatrix();
+// SpiralePath();
+// WriteAnswer();
+
+// 
+// 
+// 
+System.Console.WriteLine("Enter a size of matrix: ");
+int m_size = int.Parse(Console.ReadLine());
+int[,] matrix = new int[m_size, m_size];
+System.Console.WriteLine("Enter a start number: ");
+int currentCount = int.Parse(Console.ReadLine());
+DrawSpiral();
+PrintMatrix();
+void DrawSpiral()
 {
-    N = int.Parse(Console.ReadLine());
-    matrix = new int[N, N];
-    for (int y = 0; y < N; y++)
+    //x,y   x, y+size-1
+    //x+1, y+size-1     x+size-1, y+size-1
+    //x+size-1, y+size-2    x+size-1, y
+    //x+size-2, y   x+1, y
+    int x = 0, y = 0, size = m_size;
+
+    while (size > 0)
     {
-        string[] line = Console.ReadLine().Split();
-        for (int x = 0; x < N; x++)
+        for (int i = y; i <= y + size - 1; i++)
         {
-            matrix[y, x] = int.Parse(line[x]);
+            matrix[x, i] = currentCount++;
         }
+
+        for (int j = x + 1; j <= x + size - 1; j++)
+        {
+            matrix[j, y + size - 1] = currentCount++;
+        }
+
+        for (int i = y + size - 2; i >= y; i--)
+        {
+            matrix[x + size - 1, i] = currentCount++;
+        }
+
+        for (int i = x + size - 2; i >= x + 1; i--)
+        {
+            matrix[i, y] = currentCount++;
+        }
+
+        x = x + 1;
+        y = y + 1;
+        size = size - 2;
     }
 }
-
-void InputTestMatrix()
+void PrintMatrix()
 {
-    N = 5;
-    // matrix = new int[5, 5] {
-    //     {1,2,3,4,5},
-    //     {4,5,6,7,8},
-    //     {7,8,9,1,2},
-    //     {8,7,6,5,3},
-    //     {4,6,7,9,8}};
-            matrix = new int[5, 5] {
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0}};
-}
-
-void SpiralePath()
-{
-    int x = 0;
-    int y = 0;
-    int sx = 1;
-    int sy = 0;
-    answer = matrix[y, x].ToString();
-    
-    matrix[y, x] = -1;
-    bool found;
-    do
+    for (int i = 0; i < m_size; i++)
     {
-        found = false;
-        while (InRange(x + sx, y + sy))
+        for (int j = 0; j < m_size; j++)
         {
-            x += sx;
-            y += sy;
-            answer += " " + matrix[y, x];
-            matrix[y, x] = -1;
-            found = true;
+            Console.Write(String.Format("{0,3}", matrix[i, j]));
         }
-        int sxTemp = sx;
-        sx = -sy;
-        sy = sxTemp;
-        System.Console.WriteLine(answer);
-        Console.ReadKey();
-    } while (found);
+        Console.WriteLine();
+    }
 }
-
-bool InRange(int x, int y)
-{
-    if (x < 0 || x >= N) return false;
-    if (y < 0 || y >= N) return false;
-    if (matrix[y, x] == -1) return false;
-    return true;
-}
-
-void WriteAnswer()
-{
-    System.Console.WriteLine(answer);
-    System.Console.ReadKey();
-}
-
-InputTestMatrix();
-SpiralePath();
-WriteAnswer();
