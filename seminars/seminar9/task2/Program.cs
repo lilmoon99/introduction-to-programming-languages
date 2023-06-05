cf85d09866384a5bfe6245ba939a9faea2cd9cda
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-05 21:58:22 +0400

    task 2 solved

--- seminars/seminar9/task2/Program.cs
+++ seminars/seminar9/task2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿/*
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+System.Console.WriteLine(RecurSum(45));
+int RecurSum(int number){
+    if (number == 0){
+        return 0;
+    } else{
+        int sum =+ number % 10;
+        return sum + RecurSum(number / 10);
+    }
+}
