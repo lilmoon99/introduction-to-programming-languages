e8f39021d0f21ccf71cf1a3024e7e89d251857f3
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-05 21:37:42 +0400

    task 1 solved

--- seminars/seminar9/task1/Program.cs
+++ seminars/seminar9/task1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 5, 6, 7, 8"
+*/
+// RecurMethod(4,8);
+void RecurMethod(int fromN, int toN)
+{
+    for (int i = fromN; i <= toN; i++)
+    {
+        System.Console.Write($"{i} ");
+    }
+}
+void rec(int start, int end)
+{
+    if (start == end) return;
+    else
+    {
+        Console.Write(start + " ");
+        start++;
+        rec(start, end);
+    }
+}
+rec(4, 8);
+
+string Recur(int start,int end){
+    if (start == end) { 
+        return start.ToString();
+    } else (start + Recur(start + 1,end));
+}
