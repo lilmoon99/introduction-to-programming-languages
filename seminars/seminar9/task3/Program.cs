a417a5225890a755c41b32b361d10d7eae3154ee
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-05 22:08:07 +0400

    task 3 solved

--- seminars/seminar9/task3/Program.cs
+++ seminars/seminar9/task3/Program.cs
@@ -3,3 +3,14 @@
 A = 3; B = 5 -> 243 (3⁵)
 A = 2; B = 3 -> 8
 */
+
+int RecurPow(int a,int b){
+    if(b <= 0){
+        return 1;
+    } else {
+        int result = a ;
+        b--;
+        return result * RecurPow(a,b);
+    }
+}
+System.Console.WriteLine(RecurPow(2,10));
