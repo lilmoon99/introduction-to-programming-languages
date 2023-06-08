0f8c59e6ff2724a00247de85a229210f84ff32a1
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-08 19:39:12 +0400

    task 1 solved

--- seminars/seminar9_homeWork/task1/Program.cs
+++ seminars/seminar9_homeWork/task1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+bool ValidateNumber(int N)
+{
+    if (N < 0)
+    {
+        System.Console.WriteLine($"{N} is negative.");
+        return false;
+    }else if(N == 0){
+        System.Console.WriteLine($"{N} is zero.");
+        return false;
+    }
+    else return true;
+}
+
+string FromNToOne(int N)
+{
+    if (N == 1) return N.ToString();
+    else
+    {
+        return (N + "," + FromNToOne(N - 1));
+    }
+}
+
+System.Console.WriteLine("Enter a positive number: ");
+int number = int.Parse(Console.ReadLine());
+if (ValidateNumber(number))
+{
+    System.Console.WriteLine(FromNToOne(number));
+}
