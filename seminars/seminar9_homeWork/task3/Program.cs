b8492d336147264f0293c894ae2c2b7ce8e83b59
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-08 20:55:27 +0400

    task 3 solved

--- seminars/seminar9_homeWork/task3/Program.cs
+++ seminars/seminar9_homeWork/task3/Program.cs
@@ -3,3 +3,12 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
+
+uint AckermannFunction(uint m,uint n){
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return AckermannFunction(m-1,1);
+    else return AckermannFunction(m-1,AckermannFunction(m,n-1));
+}
+uint arg1 = 3;
+uint arg2 = 5;
+System.Console.WriteLine($"Result of Ackermann function work for m = {arg1}, n = {arg2} is {AckermannFunction(arg1,arg2)}");
