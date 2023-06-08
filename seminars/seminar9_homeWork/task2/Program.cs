8848d367dbef89d0c37db1a9fe95518c7d853be4
Aynur Shamsullin <lilmoon99@ya.ru>
2023-06-08 19:51:19 +0400

    task 2 solved

--- seminars/seminar9_homeWork/task2/Program.cs
+++ seminars/seminar9_homeWork/task2/Program.cs
@@ -3,3 +3,23 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+
+int SumFromMToN(int M, int N)
+{
+    if (M > N) return 0;
+    else
+    {
+        return (M + SumFromMToN(M + 1, N));
+    }
+}
+
+int InitNumber(string arg)
+{
+    System.Console.WriteLine($"Enter {arg}: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int M = InitNumber("M");
+int N = InitNumber("N");
+System.Console.WriteLine($"Result of adding numbers from {M} to {N} is {SumFromMToN(M, N)}");
